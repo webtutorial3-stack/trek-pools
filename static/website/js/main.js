2cc48eb5ac0b784cd70f82e803e533e7a53d0c96
unknown <you@example.com>
2020-06-29 20:25:28 +0100

    initial commit

--- static/website/js/main.js
+++ static/website/js/main.js
@@ -164,3 +164,54 @@
     });
 
 })(jQuery);
+
+
+
+/*====================================================
+                        NAVIGATION
+====================================================*/
+// Show/Hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            // show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
+
+// Close mobile menu on click
+$(function(){
+
+    $(".navbar-collapse ul li a").on("click touch", function(){
+
+        $(".navbar-toggler").click();
+    });
+});
+
+
+
+
